352fa388c514c9537827d0de71d2b53876a7c5e7
Michelle Anthony <michelle.a.anthony@gmail.com>
2016-03-09 18:02:38 -0800

    AI works but is v bad

--- player.cpp
+++ player.cpp
@@ -2,24 +2,44 @@
 
 /*
  * Constructor for the player; initialize everything here. The side your AI is
- * on (BLACK or WHITE) is passed in as "side". The constructor must finish 
+ * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
  */
-Player::Player(Side side) {
+Player::Player(Side s) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
 
-    /* 
+    /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
      * 30 seconds.
      */
+
+    this->board = new Board();
+    this->side = s;
 }
 
 /*
  * Destructor for the player.
  */
 Player::~Player() {
+}
+
+std::vector<Move> Player::getValidMoveList () {
+
+    std::vector<Move> v;
+    //iterate through all positions of board TODO improve efficiency? we don't have to iterate though all positions, really... but then the board is small
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            Move m(i,j);
+            if (board->checkMove(&m, this->side)) {
+                // add to list
+                v.push_back(m);
+            }
+        }
+    }
+
+    return v;
 }
 
 /*
@@ -34,10 +54,34 @@ Player::~Player() {
  * The move returned must be legal; if there are no valid moves for your side,
  * return NULL.
  */
-Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /* 
+Move * Player::doMove(Move *opponentsMove, int msLeft) {
+    /*
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
-     */ 
-    return NULL;
+     */
+    // timeLeft -= msLeft;
+
+    if (opponentsMove) { // not null
+        Side other = (side == BLACK) ? WHITE : BLACK;
+        board->doMove(opponentsMove, other);
+    }
+
+    // TODO include condition for unlimited time or otherwise
+    // but for now whatever
+
+    std::vector<Move> movelist = getValidMoveList();
+
+    if (movelist.size() == 0)
+        return NULL; // no available moves
+
+    //for now, play the first move
+    board->doMove(&movelist[0], this->side);
+
+    int x = movelist[0].getX();
+    int y = movelist[0].getY();
+
+    Move *m = new Move(x,y);
+
+    return m;
+
 }
--- player.h
+++ player.h
@@ -2,6 +2,7 @@
 #define __PLAYER_H__
 
 #include <iostream>
+#include <vector>
 #include "common.h"
 #include "board.h"
 using namespace std;
@@ -11,11 +12,15 @@ class Player {
 public:
     Player(Side side);
     ~Player();
-    
+
     Move *doMove(Move *opponentsMove, int msLeft);
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
+private:
+    Side side;
+    Board *board;
+    std::vector<Move> getValidMoveList();
 };
 
 #endif
